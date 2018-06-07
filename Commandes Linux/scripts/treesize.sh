62fe612d3e3b232e4bab8fc980289c278b02f176
DamienDeberthe <37943504+DamienDeberthe@users.noreply.github.com>
2018-06-07 10:31:32 +0200

    Create treesize.sh

---
+++ Commandes Linux/scripts/treesize.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+echo -e "\n**************************************\n************** TREESIZE **************\n**************************************\n"
+
+read -p "Quel dossier analyser? (ex: '/home') : " dir
+
+echo -e "\n--------------------------------------"
+du $dir -k --max-depth=1 --time | sort -nr | awk '
+     BEGIN {
+        split("KB,MB,GB,TB", Units, ",");
+     }
+     {
+        u = 1;
+        while ($1 >= 1024) {
+           $1 = $1 / 1024;
+           u += 1
+        }
+        $1 = sprintf("%.1f %s", $1, Units[u]);
+        print $0;
+     }
+    '
+echo -e "--------------------------------------\n"
