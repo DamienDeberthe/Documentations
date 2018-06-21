c5f8b4dc0d3e5621d7f3989023ab47058eb07ba7
DamienDeberthe <37943504+DamienDeberthe@users.noreply.github.com>
2018-06-21 16:35:13 +0200

    Update docker-install-compose.sh

--- Docker/Scripts/docker-install-compose.sh
+++ Docker/Scripts/docker-install-compose.sh
@@ -1,4 +1,6 @@
-apt-get install curl
+#/bin/bash
+
+apt-get update && apt-get install curl
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
