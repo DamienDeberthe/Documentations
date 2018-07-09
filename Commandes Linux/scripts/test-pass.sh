e926b880640fdc1b052bed150a6c99caf4bccd8c
DamienDeberthe <37943504+DamienDeberthe@users.noreply.github.com>
2018-07-09 15:43:53 +0200

    Create test-pass.sh

---
+++ Commandes Linux/scripts/test-pass.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### USE THIS SCRIPT ON A KALI LINUX OR INSTALL PATATOR COMMAND ###
+
+
+#--- Indiquer le nom d'utilisateur et pass ---#
+read -p "Entrez le login de connexion a tester : " user
+read -p "Entrez le mot de passe de connexion a tester : " mdp
+
+#--- Indiquer les adresses IP des serveurs a tester dans le fichier inc/ip.txt ---#
+
+for line in $(cat ip.txt)
+do
+echo -e "----------"
+echo -e "Test des id: $user/$user sur le serveur : $line\n"
+patator ssh_login user=$user password=$mdp host=$line -x ignore:fgrep='Authentication failed.'
+echo -e "----------\n\n"
+
+#Pour utiliser un dictionnaire contenu dans un fichier, voici la commande a remplacer
+#patator ssh_login user=$user password=FILE0 0=/opt/comptes_QZ/inc/mdp.txt host=$line -x ignore:fgrep='Authentication failed.'
+
+done
