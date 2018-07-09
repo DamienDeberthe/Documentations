be7b6cd71a0bd81faf5420b83a092e4c6949a0b2
DamienDeberthe <37943504+DamienDeberthe@users.noreply.github.com>
2018-07-09 15:48:04 +0200

    Update test-pass.sh

--- Commandes Linux/scripts/test-pass.sh
+++ Commandes Linux/scripts/test-pass.sh
@@ -5,17 +5,17 @@
 
 #--- Indiquer le nom d'utilisateur et pass ---#
 read -p "Enter a login : " user
-read -p "Enter the pass : " mdp
+read -p "Enter the pass : " pass
 
 #--- Indiquer les adresses IP des serveurs a tester dans le fichier inc/ip.txt ---#
 for line in $(cat ip.txt)
 do
 echo -e "----------"
-echo -e "Test des id: $user/$mdp sur le serveur : $line\n"
-patator ssh_login user=$user password=$mdp host=$line -x ignore:fgrep='Authentication failed.'
+echo -e "Test des id: $user/$pass sur le serveur : $line\n"
+patator ssh_login user=$user password=$pass host=$line -x ignore:fgrep='Authentication failed.'
 echo -e "----------\n\n"
 
 #Pour utiliser un dictionnaire contenu dans un fichier, voici la commande a remplacer
-#patator ssh_login user=$user password=FILE0 0=/opt/comptes_QZ/inc/mdp.txt host=$line -x ignore:fgrep='Authentication failed.'
+#patator ssh_login user=$user password=FILE0 0=/opt/comptes_QZ/inc/pass.txt host=$line -x ignore:fgrep='Authentication failed.'
 
 done
