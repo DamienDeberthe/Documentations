275ec20bce148a659b44da2d510f268eee1bbb00
DamienDeberthe <37943504+DamienDeberthe@users.noreply.github.com>
2019-01-16 16:32:53 +0100

    Update api.js

--- Node.js/API/api.js
+++ Node.js/API/api.js
@@ -1,45 +1,57 @@
 //L'application requiert l'utilisation du module Express.
 //La variable express nous permettra d'utiliser les fonctionnalités du module Express.
 var express = require('express');
-// Nous définissons ici les paramètres du serveur.
+// Paramètres serveur
 var hostname = 'localhost';
 var port = 3000;
 
-// Nous créons un objet de type Express. 
+// Creation objet Express 
 var app = express();
 
 //Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
 //C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes.
 var myRouter = express.Router();
 
-//ROUTES
 
-//Route '/'
+
+// -----------------------------------------------------------------
+// ROUTES
+
+// Route '/'
 myRouter.route('/')
 // all permet de prendre en charge toutes les méthodes. 
-.all(function(req,res){ 
-      res.json({message : "Welcome to API", methode : req.method});
-});
+.all(function(req,res){ res.json({message : "Welcome to API", methode : req.method}); });
+
 
 // Route '/r1'
 myRouter.route('/r1')
-// J'implémente les méthodes GET, PUT, UPDATE et DELETE
+// Implémente méthodes GET, PUT, UPDATE, DELETE
 // GET
-.get(function(req,res){
-	  res.json({message : "Get r1", methode : req.method});
-})
+.get(function(req,res){ res.json({message : "Get r1", methode : req.method}); })
 //POST
-.post(function(req,res){
-      res.json({message : "Post r1", methode : req.method});
-})
+.post(function(req,res){ res.json({message : "Post r1", methode : req.method}); })
 //PUT
-.put(function(req,res){
-      res.json({message : "Put r1", methode : req.method});
-})
+.put(function(req,res){ res.json({message : "Put r1", methode : req.method}); })
 //DELETE
-.delete(function(req,res){
-res.json({message : "Delete r1", methode : req.method});
-});
+.delete(function(req,res){ res.json({message : "Delete r1", methode : req.method}); });
+
+
+// Route '/r2'
+myRouter.route('/r2')
+// Implémente méthodes GET, PUT, UPDATE, DELETE
+// GET
+.get(function(req,res){ res.json({message : "Get r2", methode : req.method}); })
+//POST
+.post(function(req,res){ res.json({message : "Post r2", methode : req.method}); })
+//PUT
+.put(function(req,res){ res.json({message : "Put r2", methode : req.method}); })
+//DELETE
+.delete(function(req,res){ res.json({message : "Delete r2", methode : req.method}); });
+
+
+
+// -----------------------------------------------------------------
+
 
 // Nous demandons à l'application d'utiliser notre routeur
 app.use(myRouter);
